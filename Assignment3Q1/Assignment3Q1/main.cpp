4e28c2af9cfc15b848ab2d18d97a4583fbe29d6c
Austin <Austin@AUSTIN-LAPTOP>
2017-03-01 18:12:26 -0600

    Begun adding functions and structs.

--- Assignment3Q1/Assignment3Q1/main.cpp
+++ Assignment3Q1/Assignment3Q1/main.cpp
@@ -1,8 +1,19 @@
+/*
+Assignment 3, Question 1 "Kruskal's Algorithm"
+Austin McPhail
+200341412
+
+*/
+
 #include <iostream>
 #include <climits> //Used for HIGH_VALUE
 
 using namespace std;
 
+struct SetNode;
+
+void KruskalsMinimumSpanningTree();
+void KruskalsMinimumSpanningTree();
 int main() {
 
 }
